ec4e705719c310aea1466302044a08c6a4a632ce
Maxximuzz <82037805+Maxximuzz@users.noreply.github.com>
2021-04-09 15:30:14 +0300

    Добавили правила для адаптива

--- js/main.js
+++ js/main.js
@@ -2,9 +2,18 @@ const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
   slidesPerView: 6,
+  spaceBetween: 20,
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next',
     prevEl: '.channel-button-prev',
   },
 });
+const searchBtn = documend.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+if (document.documentElement.scrollWidth <= 640) {
+  myChannelSlider.destroy();
+}
